4cffdd21be1f982b356b80825700527996547ac8
Ralds Oril <robertjswallow@gmail.com>
2020-12-11 14:17:29 -0800

    added questions display

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // set variables
+
+// 
 var startBtnEl = document.querySelector("#startBtn");
 var testEl = document.getElementById("test");
+let questionNumber = 0;
 
 
 // function to start timer and count down
@@ -36,48 +39,48 @@ startBtnEl.addEventListener("click", function(){
 
 })
 
-// function myFunction() {
-//     var x = document.getElementById("test");
-//     if (x.style.display === "none") {
-//         x.style.display = "block";
-//     } else {
-//         x.style.display = "none";
-//     }
-// }
+function newQuestion() {
+let currentQ = myQuestions[questionNumber]
+document.getElementById("question").textContent = currentQ.question
+document.getElementById("answer1").textContent = currentQ.answers[0]
+document.getElementById("answer2").textContent = currentQ.answers[1]
+document.getElementById("answer3").textContent = currentQ.answers[2]
+document.getElementById("answer4").textContent = currentQ.answers[3]
+}
 
+questionsarray;
+let myQuestions = [
+    {
+        question: "Bulbasaur is a grass type pokemon. Bulbasaur is weak against fire. Which attack would be super effective?",
+        answers: [
+         "Flamethrower",
+         "Watergun",
+         "Thunderbolt",
+         "Growl",
+        ],
+        correctAnswer: "Flamethrower"
+    },
+    {
+        question: "Charmander is a fire type pokemon. Charmander is weak against water. Which attack would be super effective?",
+        answers: [
+             "Flamethrower",
+             "Watergun",
+             "Thunderbolt",
+             "Growl",
+    ],
+        correctAnswer: "Watergun"
+    },
+    {
+        question: "Squirtle is a water type pokemon. Squirtle is weak against electricity. Which attack would be super effective?",
+        answers: [
+            "Flamethrower",
+            "Watergun",
+            "Thunderbolt",
+            "Growl",
 
+        ],
 
-// questions array
-// let myQuestions = [
-//     {
-//         question: "Bulbasaur is a grass type pokemon. Bulbasaur is weak against fire. Which attack would be super effective?",
-//         answers: {
-//             a: "Flamethrower",
-//             b: "Watergun",
-//             c: "Thunderbolt",
-//             d: "Growl"
-//         },
-//         correctAnswer: "a"
-//     },
-//     {
-//         question: "Charmander is a fire type pokemon. Charmander is weak against water. Which attack would be super effective?",
-//         answers: [
-//             a: "Flamethrower",
-//             b: "Watergun",
-//             c: "Thunderbolt",
-//             d: "Growl"
-//         ]
-//         correctAnswer: "b"
-//     },
-//     {
-//         question: "Squirtle is a water type pokemon. Squirtle is weak against electricity. Which attack would be super effective?",
-//         answers: [
-//             "Flamethrower",
-//             "Watergun",
-//             "Thunderbolt",
-//             "Growl"
-//         ],
-//         correctAnswer: 2
-//     }
-// ];
+        correctAnswer: "Thunderbolt"
+    }
+];
 
