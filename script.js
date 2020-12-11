57c8d27a51a06fd0d1e170cfebfd2c8484472468
Ralds Oril <robertjswallow@gmail.com>
2020-12-11 15:05:39 -0800

    got questions to dicplay,  cycle and loop back

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 
 // 
 var startBtnEl = document.querySelector("#startBtn");
+var ansBtn = document.getElementById("answer-buttons");
 var testEl = document.getElementById("test");
 let questionNumber = 0;
-
+let scoreCount = 0;
+var counter = 30;
 
 // function to start timer and count down
 function start() {
-    var counter = 30;
     setInterval(function () {
         counter--;
         if (counter >= 0) {
@@ -36,7 +37,7 @@ function hide() {
 startBtnEl.addEventListener("click", function(){
     hide();
     start();
-
+    newQuestion();
 })
 
 function newQuestion() {
@@ -48,8 +49,25 @@ document.getElementById("answer3").textContent = currentQ.answers[2]
 document.getElementById("answer4").textContent = currentQ.answers[3]
 }
 
-questionsarray;
-let myQuestions = [
+ansBtn.addEventListener("click", checkAnswer);
+function checkAnswer(el){
+    console.log(el);
+let choice = el.textContent;
+console.log(choice)
+if (choice == myQuestions[questionNumber].correctAnswer) {
+scoreCount = scoreCount + 1
+} else { counter = counter - 5
+
+}
+questionNumber ++
+if (questionNumber >= myQuestions.length){
+     questionNumber = 0
+}
+newQuestion()
+}
+
+
+var myQuestions = [
     {
         question: "Bulbasaur is a grass type pokemon. Bulbasaur is weak against fire. Which attack would be super effective?",
         answers: [
